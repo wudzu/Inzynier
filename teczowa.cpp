c8bf5c6a4f8a346b9a2cec23c76f73fa28dd78a0
AndyMaster <andy356@wp.pl>
2015-03-21 12:34:26 +0100

    Update teczowa.cpp

--- teczowa.cpp
+++ teczowa.cpp
@@ -9,7 +9,7 @@ void teczowa16::tworz()
 {
     printf("\nPodaj t: ");
     scanf("%d", &t);
-    printf("\nPodaj m (wskazane przemnozyc m przez wartosc t): ");
+    printf("\nPodaj m: ");
     scanf("%d", &m);
     printf("\nPodaj plaintext (hexadecymalnie): ");
     scanf("%x", &plaintext);
@@ -81,4 +81,68 @@ void teczowa16::menuTeczowa()
     printf("\n\n%d\n\n",statystyka());
     printf("%d", pudla);
 }
+
+void teczowa16::menuTeczowaZapis()
+{
+    unsigned int pom[3],pom4,pom6,rodzaj,wzrost,krok,teoria;
+
+    printf("\nPodaj t: ");
+    scanf("%d", &pom[0]);
+
+    printf("\nPodaj m: ");
+    scanf("%d", &pom[1]);
+
+    printf("\nZachowac stosunek rozmiaru macierzy m*t?\n0 - tak\n1 - nie\n");
+    scanf("%d", &teoria);
+
+    printf("Podaj plaintext (hexadecymalnie): ");
+    scanf("%x", &plaintext);
+
+    printf("Podaj seed liczb losowych: ");
+    scanf("%d", &pom4);
+    srand(pom4);
+
+    printf("Ktora zmienna ma rosnac:\n0 - t\n1 - m\n");
+    scanf("%d", &rodzaj);
+    printf("O ile ma sie ostatecznie zwiekszyc?\n");
+    scanf("%d", &wzrost);
+    printf("Z jakim krokiem?\n");
+    scanf("%d", &krok);
+    printf("Ile testow na zestaw?\n");
+    scanf("%d", &pom6);
+    FILE* output;
+    output=fopen("dane.txt","wt");
+    fprintf(output,"Plaintext testu to ");
+        fprintf(output,"%x",plaintext);
+
+    fprintf(output,"\nSeed liczb losowych to %d\n", pom4);
+    fprintf(output,"\nLiczba zestawow to %d\n", wzrost);
+    wzrost+=pom[rodzaj];
+    while(pom[rodzaj]<wzrost)
+    {
+        if(!teoria)
+        {
+            pom[2]=pom[0]*pom[1];
+        }
+        else
+        {
+            pom[2]=pom[1];
+        }
+
+        fprintf(output,"Zestaw %d, %d\n",pom[0],pom[2]);
+        printf("Zestaw %d, %d\n",pom[0],pom[2]);
+        for (int i=0;i<pom6;i++)
+        {
+            tworz(pom[0],pom[2],plaintext);
+            fprintf(output,"%d\t",statystyka());
+            fprintf(output,"%d\n",pudla);
+            printf("%d\t",statystyka());
+            printf("%d\n",pudla);
+        }
+        pom[rodzaj]+=krok;
+    }
+    fclose(output);
+
+}
+
 
