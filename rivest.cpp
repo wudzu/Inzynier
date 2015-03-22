dcfd51e45661734a5b561679a05e80a70274f6f9
AndyMaster <andy356@wp.pl>
2015-03-22 13:05:31 +0100

    Poprawki zapisu

--- rivest.cpp
+++ rivest.cpp
@@ -126,7 +126,7 @@ void rivest16::menuRivestZapis()
         fprintf(output,"%x",plaintext);
 
     fprintf(output,"\nSeed liczb losowych to %d\n", pom4);
-    fprintf(output,"\nLiczba zestawow to %d\n", wzrost);
+    fprintf(output,"\nLiczba zestawow to %d\n", (wzrost/krok));
     wzrost+=pom[rodzaj];
     while(pom[rodzaj]<wzrost)
     {
